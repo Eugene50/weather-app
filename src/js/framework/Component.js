464d4a3cc175eeebcbf1452a41d3203752fdfd4d
Eugene50 <nova_alf@ukr.net>
2019-04-24 16:36:06 +0300

    add new method

--- src/js/framework/Component.js
+++ src/js/framework/Component.js
@@ -1,27 +1,38 @@
 export default class Component {
-   constructor(host, props = {}) {
-      this.host = host;
-      this.props = props;
-      this._render();
-   }
-   _render() {
-      this.host.innerHTML = "";
-      const content = this.render();
-      
-      if(typeof content === 'string'){
-         this.host.innerHTML = content;
-      } else {
-         content.map(item => {
-            if(typeof item === "string"){
-               const htmlElemnt = document.createElement("div");
-               htmlElemnt.innerHTML = item;
-               return htmlElemnt;
-            } else {
-               return item;
-            }
-         }).forEach(htmlElement => {
-            this.host.appendChild(htmlElement);
-         });
+  constructor(host, props = {}) {
+    this.host = host;
+    this.props = props;
+    this._render();
+  }
+  _render() {
+    this.host.innerHTML = "";
+    const content = this.render();
+
+    if (typeof content === "string") {
+      this.host.innerHTML = content;
+    } else {
+      content
+        .map(this._vDomPrototypeElementToHtmlElement)
+        .forEach(htmlElement => {
+          this.host.appendChild(htmlElement);
+        });
+    }
+  }
+
+  _vDomPrototypeElementToHtmlElement(element) {
+    if (typeof element === "string") {
+      const htmlElemnt = document.createElement("div");
+      htmlElemnt.innerHTML = element;
+      return htmlElemnt;
+    } else {
+      if (typeof element.tag === "function") {
+        const container = document.createElement("div");
+        new element.tag(container, element.props);
+        return container;
       }
-   }
+      return element;
+    }
+  }
 }
+
+
