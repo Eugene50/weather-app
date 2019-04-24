87f9bacaa7e2068445959d955907774778d93c61
Eugene50 <nova_alf@ukr.net>
2019-04-24 16:37:57 +0300

    upd code

--- src/js/Components/SearchBar/SearchBar.js
+++ src/js/Components/SearchBar/SearchBar.js
@@ -2,6 +2,6 @@ import Component from "../../framework/Component";
 
 export default class SearchBar extends Component {
   render() {
-    return "I`m SearchBar component";
+    return "I`m SearchBar component" + " " + this.props.search;
   }
 }
