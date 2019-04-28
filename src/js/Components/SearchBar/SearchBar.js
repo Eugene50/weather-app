329f3b21f2cd160a853c4abff40c938fb263cc7c
Eugene50 <nova_alf@ukr.net>
2019-04-29 02:01:16 +0300

    add new props

--- src/js/Components/SearchBar/SearchBar.js
+++ src/js/Components/SearchBar/SearchBar.js
@@ -2,6 +2,48 @@ import Component from "../../framework/Component";
 
 export default class SearchBar extends Component {
   render() {
-    return "I`m SearchBar component" + " " + this.props.search;
+    return [
+     {
+       tag: "div",
+       classList: ["search-bar-wrapper"],
+       children: [
+        {
+          tag: "button",
+          classList: ["button"],
+        },
+        {
+          tag: "button",
+          classList: ["button"],
+        },
+        {
+          tag: "input",
+          classList: ["search"],
+          
+        },
+        {
+          tag: "button",
+          classList: ["search-btn", "button"],
+          attributes: [
+            {
+              name: "name",
+              value: "search",
+            },
+            {
+              name: "id",
+              value: "search-btn",
+            },
+            {
+              name: "value",
+              value: "Search",
+            },
+            {
+              name: "type",
+              value: "submit",
+            },
+          ]
+        },
+       ]
+     }
+    ];
   }
 }
