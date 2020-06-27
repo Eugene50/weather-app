d3a978c2340490bc0e2338c6cfe825156d46d80e
Eugene50 <nova_alf@ukr.net>
2020-06-28 01:31:32 +0300

    add two methods

--- src/Services/WeatherDataService.js
+++ src/Services/WeatherDataService.js
@@ -1,8 +1,27 @@
-class WeatherDataService{
-   getCurrentWeather() {
-   }
-   getWeatherForecast() {
-   }
- }
- 
- export default new WeatherDataService();
+import { commonWeatherData } from "../Services/api.js";
+
+class WeatherDataService {
+  constructor(props){
+    this.props = props;
+  }
+
+  getCurrentWeather() {
+    return commonWeatherData().then((data) => {
+      return data
+        .filter((item, index) => {
+          if (index === 0) {
+            return item;
+          }
+        })
+        .map((item) => {
+          return this.props =  item.temp;
+        });
+    })
+    //тут идет выборка ветер, температура, влажность итдю
+  }
+
+
+  getWeatherForecast() {}
+}
+
+export default new WeatherDataService();
