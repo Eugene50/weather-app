9259345d4843ce9c88feec20b1a4513c93f74abd
Eugene50 <nova_alf@ukr.net>
2020-06-28 01:39:30 +0300

    delete some imports

--- src/js/Components/App/App.js
+++ src/js/Components/App/App.js
@@ -1,10 +1,10 @@
-import { CurrentWeather } from "../CurrentWeather";
-import Component from "../../framework/Component";
-import { Temperature } from "../Temperature";
-import { SearchBar } from "../SearchBar";
+import {CurrentWeather} from "../CurrentWeather";
+import  Component from "../../framework/Component";
+import {SearchBar} from "../SearchBar";
 import {WeatherForecast} from "../WeatherForecast";
 
 export default class App extends Component {
+  
   render() {
     return [
       {
@@ -22,13 +22,3 @@ export default class App extends Component {
     ];
   }
 }
-
-/* const url = 'https://api.openweathermap.org/data/2.5/weather?q=Kyiv,ua&appid=4fd70ad4824fb22274880ed243c351df';
-
-const getApi = url => 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      });
- */
