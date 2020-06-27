9ac39d015cd16f76b8cb5ede1f4e393b1224664c
Eugene50 <nova_alf@ukr.net>
2020-06-28 01:40:18 +0300

    add new component to render method

--- src/js/Components/CurrentWeather/CurrentWeather.js
+++ src/js/Components/CurrentWeather/CurrentWeather.js
@@ -1,9 +1,21 @@
 import rain from "../../../img/rain.png";
+import { Temperature } from "../Temperature";
+import WeatherDataService from "../../../Services/WeatherDataService.js";
+//import commonWeatherData from "../../../Services/api.js";
 
 import Component from "../../framework/Component";
 
 export default class CurrentWeather extends Component {
   render() {
-    return [];
+    return [
+      {
+        tag: Temperature,
+        props: WeatherDataService.getCurrentWeather(),
+      },
+    ];
   }
+
+  
 }
+
+//props from  weatherDataService must have
