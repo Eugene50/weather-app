7c7332c740444166975540f76c40a09bd375c4ea
Eugene50 <nova_alf@ukr.net>
2020-06-28 01:37:38 +0300

    add  rendering 5 Temperature components

--- src/js/Components/WeatherForecast/WeatherForecast.js
+++ src/js/Components/WeatherForecast/WeatherForecast.js
@@ -1,12 +1,31 @@
 import Component from "../../framework/Component";
+import WeatherDataService from "../../../Services/WeatherDataService.js";
+import { Temperature } from "../Temperature";
 
 export default class WeatherForecast extends Component {
   render() {
     return [
       {
-        tag: "h1",
-        classList
+        tag: Temperature,
+        props: WeatherDataService.getWeatherForecast(),
+      },
+      {
+        tag: Temperature,
+        props: WeatherDataService.getWeatherForecast(),
+      },{
+        tag: Temperature,
+        props: WeatherDataService.getWeatherForecast(),
+      },
+      {
+        tag: Temperature,
+        props: WeatherDataService.getWeatherForecast(),
+      },
+      {
+        tag: Temperature,
+        props: WeatherDataService.getWeatherForecast(),
       }
     ];
   }
 }
+
+//props from  weatherDataService must have
